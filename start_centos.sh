ee8023d20ad56838a5a7224f871c97656254f96d
lilinj2000 <lilinj2000@gmail.com>
2016-07-04 11:55:16 +0000

    updated.

--- start_centos.sh
+++ start_centos.sh
@@ -1,3 +1,3 @@
-#! /bin/bash
+#! /bin/sh
 
-sudo docker run --rm -t -i -p 8000:8000 -v $HOME:/root lilinj2000/centos-dev:latest
+docker run --rm -t -i -p 8000:8000 -v $HOME:/root lilinj2000/centos-dev:latest
