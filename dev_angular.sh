932701401e17d713faeda1a0f1e1b10140330d94
lilinj2000 <lilinj2000@gmail.com>
2018-03-06 13:07:04 +0800

    dev_angular.sh added.

--- dev_angular.sh
+++ dev_angular.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-docker run -d --rm -i -h angular -v ~:/root lilinj2000/dev:angular /bin/bash
+docker run -d --rm -i -h angular -p 32780:4200 -v ~:/root lilinj2000/dev:angular /bin/bash
