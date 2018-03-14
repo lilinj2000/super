156553efdb5099a1d0e03b3be5ef2162cfa3b334
lilinj2000 <lilinj2000@gmail.com>
2018-03-14 17:32:48 +0800

    new dev added.

--- dev_centos6.gcc.sh
+++ dev_centos6.gcc.sh
@@ -1,15 +1,13 @@
 #! /bin/sh
 
 docker run -d --rm -i \
-	-h angular \
+	-h centos6 \
 	-u 1000:1000 \
 	-w /home/llj \
 	-e USER=llj \
 	-e HOME=/home/llj \
-	-e PS1="[dev@angular \w]\$ " \
-	-p 32780:4200 \
+	-e PS1="[dev@centos6.gcc \w]\$ " \
 	-v ~:/home/llj \
 	-v /etc/passwd:/etc/passwd:ro \
 	-v /etc/group:/etc/group:ro \
-	lilinj2000/dev:angular \
-	/bin/bash
+	lilinj2000/dev:centos6.gcc
--- dev_centos6.sh
+++ dev_centos6.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
 
-docker run -d --rm -i -h centos6 -v ~:/root lilinj2000/dev:centos6
+docker run -d --rm -i \
+	-h centos6 \
+	-u 1000:1000 \
+	-w /home/llj \
+	-e HOME=/home/llj \
+	-e PS1="[dev@centos6 \w]\$ " \
+	-v ~:/home/llj \
+	lilinj2000/dev:centos6
