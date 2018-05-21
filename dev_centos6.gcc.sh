e27be7d9c2d510d320de65dd46c1ac7bd8e2b1f7
lilinj2000 <lilinj2000@gmail.com>
2018-05-21 13:44:00 +0800

    dev_centos6.gcc.sh updated for docker in windows.

--- dev_centos6.gcc.sh
+++ dev_centos6.gcc.sh
@@ -3,11 +3,14 @@
 docker run -d --rm -i \
 	-h centos6 \
 	-u 1000:1000 \
-	-w /home/llj \
+	-w //home/llj \
 	-e USER=llj \
-	-e HOME=/home/llj \
+	-e HOME=//home/llj \
 	-e PS1="[dev@centos6.gcc \w]\$ " \
-	-v ~:/home/llj \
-	-v /etc/passwd:/etc/passwd:ro \
-	-v /etc/group:/etc/group:ro \
+	-v //c/docker:/home/llj \
+	-v //c/docker/src/super/passwd:/etc/passwd:ro \
+	-v //c/docker/src/super/group:/etc/group:ro \
 	lilinj2000/dev:centos6.gcc
+
+	
+	
