c09872365629a24673cd19801844a3ef92f6c8ba
lilinj2000 <lilinj2000@gmail.com>
2018-05-23 10:57:35 +0800

    updated, docker ce for windows.

--- dev_centos6.gcc.sh
+++ dev_centos6.gcc.sh
@@ -2,15 +2,15 @@
 
 docker run -d --rm -i \
 	-h centos6 \
-	-u 1000:1000 \
-	-w //home/llj \
-	-e USER=llj \
-	-e HOME=//home/llj \
 	-e PS1="[dev@centos6.gcc \w]\$ " \
-	-v //c/docker:/home/llj \
-	-v //c/docker/src/super/passwd:/etc/passwd:ro \
-	-v //c/docker/src/super/group:/etc/group:ro \
+	-v //c/docker:/root \
 	lilinj2000/dev:centos6.gcc
 
+	# -v //c/docker/src/super/passwd:/etc/passwd:ro \
+	# -v //c/docker/src/super/group:/etc/group:ro \
+	# -u 1000:1000 \
+	# -w //home/llj \
+	# -e USER=llj \
+	# -e HOME=//home/llj \
 	
 	
