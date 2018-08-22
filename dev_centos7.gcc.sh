74a4053213b6e2156dce3e5c8166e66f6b2b2dcd
lilinj2000 <lilinj2000@gmail.com>
2018-08-22 13:55:14 +0800

    dev centos7.gcc added.

---
+++ dev_centos7.gcc.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+docker run -d --rm -i \
+	-h centos7 \
+	-e PS1="[dev@centos7.gcc \w]\$ " \
+	-v //c/docker:/root \
+	lilinj2000/dev:centos7.gcc
+
+	# -v //c/docker/src/super/passwd:/etc/passwd:ro \
+	# -v //c/docker/src/super/group:/etc/group:ro \
+	# -u 1000:1000 \
+	# -w //home/llj \
+	# -e USER=llj \
+	# -e HOME=//home/llj \
+	
+	
